143ab80705c0d2b8cbebf80fd1c339587a52f23e
Paul Beaudoin <contact@paulbeaudoin.com>
2021-12-02 11:22:33 -0500

    Add terraform state to s3

--- terraform-resources.tf
+++ terraform-resources.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket  = "nypl-travis-builds-qa"
+    key     = "discovery-hybrid-indexer-terraform-state"
+    region  = "us-east-1"
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
 }
