029eaca194affa578ed7ef750e968c709f85a129
Paul Beaudoin <contact@paulbeaudoin.com>
2022-05-04 14:38:23 -0400

    Fix bug where circ bibs weren't being deleted
    Fixes bug where circ bibs weren't deleted in the index after sneaking in
    there (likely because at some point in the past they were classified as
    Research by virtue of having 0 items). This update principally modifies
    the method for filtering out non-research bibs early in the event
    handling. Rather than simply remove non-research bibs from the list of
    bibs to reindex, the "filterOut" method now issues a DELETE call on the
    circ bibids - on the off chance that they were erroneously indexed in
    the past.

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv')
+const sinon = require('sinon')
 
+const discoveryApiIndex = require('discovery-api-indexer/lib/index')
 const logger = require('../lib/logger')
+const kmsHelper = require('../lib/kms-helper')
 
 dotenv.config({ path: './config/test.env' })
 
@@ -9,4 +12,33 @@ global.expect = require('chai').expect
 before(() => {
   // Ensure logger respects configured loglevel before running any tests:
   logger.setLevel(process.env.LOGLEVEL || 'error')
+
+  // Let's generally stub index writes and deletes across all tests
+  sinon.stub(discoveryApiIndex.resources, 'save')
+    .callsFake((indexName, records, update) => {
+      global.indexedDocuments = global.indexedDocuments.concat(records)
+      return Promise.resolve()
+    })
+
+  sinon.stub(discoveryApiIndex.resources, 'delete')
+    .callsFake((indexName, uri) => {
+      global.deletedUris = global.deletedUris.concat(uri)
+      return Promise.resolve()
+    })
+
+  sinon.stub(kmsHelper, 'decrypt').callsFake(() => Promise.resolve('decrypted!'))
+
+  global.indexedDocuments = []
+  global.deletedUris = []
+})
+
+after(() => {
+  kmsHelper.decrypt.restore()
+  discoveryApiIndex.resources.save.restore()
+  discoveryApiIndex.resources.delete.restore()
+})
+
+afterEach(() => {
+  global.indexedDocuments = []
+  global.deletedUris = []
 })
